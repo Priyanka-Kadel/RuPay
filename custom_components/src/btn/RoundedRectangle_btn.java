97235562dc6263f3667f6cc11f4c82f786189a78
Rohan <roahhanchaulagain@gmail.com>
2023-06-06 23:15:59 +0545

    added custom components

---
+++ custom_components/src/btn/RoundedRectangle_btn.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package btn;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JButton;
+
+public class RoundedRectangle_btn extends JButton {
+    
+    private Color backgroundColor;
+    private Color hoverColor;
+    private Color pressedColor;
+    
+    public RoundedRectangle_btn() {
+        super();
+        setOpaque(false);
+        setContentAreaFilled(false);
+        setBorderPainted(false);
+        setPreferredSize(new Dimension(100, 30));
+        setFocusPainted(false);
+        
+        backgroundColor = new Color(240, 240, 240);
+        hoverColor = new Color(220, 220, 220);
+        pressedColor = new Color(200, 200, 200);
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        if (getModel().isPressed()) {
+            g2.setColor(pressedColor);
+        } else if (getModel().isRollover()) {
+            g2.setColor(hoverColor);
+        } else {
+            g2.setColor(backgroundColor);
+        }
+        
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+        g2.dispose();
+        
+        super.paintComponent(g);
+    }
+}
