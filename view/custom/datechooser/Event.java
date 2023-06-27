82c5d5d76b6f88b390926d3029638c10143c562c
Rohan <roahhanchaulagain@gmail.com>
2023-06-27 23:25:27 +0545

    delete

--- view/custom/datechooser/Event.java
+++
@@ -1,8 +0,0 @@
-package view.custom.datechooser;
-
-import java.awt.event.MouseEvent;
-
-public interface Event {
-
-    public void execute(MouseEvent evt, int num);
-}
--- view/custom/datechooser/EventDateChooser.java
+++
@@ -1,6 +0,0 @@
-package view.custom.datechooser;
-
-public interface EventDateChooser {
-
-    public void dateSelected(SelectedAction action, SelectedDate date);
-}
